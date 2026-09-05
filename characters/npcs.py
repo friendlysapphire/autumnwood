74aff0bfaaba7f33325f83a6ba94f47dc862a8e5
Aaron Weiss <28325936+friendlysapphire@users.noreply.github.com>
2026-09-05 10:29:15 -0700

    add interaction rects to NPC vendors

--- characters/npcs.py
+++ characters/npcs.py
@@ -1,6 +1,8 @@
 from enum import StrEnum
 
-from characters.character import Character, DirectionValue
+import pygame
+
+from characters.character import Character, DirectionValue, AnimationState
 from characters.character_scaffolds import CharacterScaffold
 
 
@@ -58,3 +60,25 @@ class NPC(Character):
                       self.initial_y_spawn_loc,
                       self.direction_x,
                       self.direction_y)
+
+    @property
+    def interaction_rect(self) -> pygame.Rect:
+
+        if not self.spawned:
+            raise ValueError(f"NPC {self.name}, {self.display_name} has no interaction rect beccause it's not spawned.")
+
+        # calculate the actual visible sillouette of the sprite 
+        top = self.world_y + self.scaffold.visible_top_offset
+        left = self.world_x + self.scaffold.visible_left_offset
+
+        # get the size of the sprite rect, 
+        npc_idle_frame = self.scaffold.sprite_animation_rects.get(AnimationState.IDLE)[0]
+        npc_sprite_full_width = npc_idle_frame.width
+        npc_sprite_full_height = npc_idle_frame.height
+
+        width = npc_sprite_full_width - self.scaffold.visible_left_offset - self.scaffold.visible_right_offset
+        height = npc_sprite_full_height - self.scaffold.visible_top_offset - self.scaffold.visible_bottom_offset
+
+        # expand equal amounts in all directions
+        return pygame.Rect(left - 25, top - 25, width + 50, height + 50)
+
